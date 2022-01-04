9ce8552dfc8fc67260551eb54f005a445d32924d
Battal Ucar <battalucar@outlook.com>
2022-01-04 23:19:06 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# battal-npm-ucar
---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("bu benim uygulamam");
+
+// npm init - package.json oluşturmak - paket oluşturmak
+// npm install - dependecy package'ları indirmek için kullanılır
+// npm install webpack-cli --save-dev -> development packageları için -> devDependency için
